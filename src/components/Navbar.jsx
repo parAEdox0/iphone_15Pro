c22b7e2228413e154dbb7a6759dfbd9c81312743
parth <paraedox0@gmail.com>
2024-07-18 16:34:22 +0530

    created nav links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,13 @@ const Navbar = () => {
   return (
     <header>
       <nav className="h-11 flex items-center justify-between px-4 lg:justify-center lg:gap-20">
-        <img src="assets/logo.png" className="h-[42%]" />
-        <div className="hidden md:flex gap-11 items-center text-sm text-zinc-400">
-          <p>Store</p>
-          <p>Mac</p>
-          <p>Entertainment</p>
-          <p>Accesories</p>
-          <p>Support</p>
+        <img src="assets/logo.png" className="h-[42%] cursor-pointer" />
+        <div className="hidden md:flex gap-11 items-center text-sm text-zinc-400 ">
+          <p className="cursor-pointer hover:text-zinc-50">Store</p>
+          <p className="cursor-pointer hover:text-zinc-50">Mac</p>
+          <p className="cursor-pointer hover:text-zinc-50">Entertainment</p>
+          <p className="cursor-pointer hover:text-zinc-50">Accesories</p>
+          <p className="cursor-pointer hover:text-zinc-50">Support</p>
         </div>
         <div className="flex items-center gap-12">
           <img src="icons/search.png" className="h-5" />
